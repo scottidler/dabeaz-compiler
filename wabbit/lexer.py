027ab260df65bcf5d2e02660f8abd89d7dd9736a
Scott Idler <scott.a.idler@gmail.com>
2019-11-14 10:11:14 -0800

    cleaned up lexer and have some sample code to run with invoking __main__

--- wabbit/lexer.py
+++ wabbit/lexer.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python3
 
 import re
+import sys
 
 from sly import Lexer
+from sly.lex import Token
 
 class WabbitLexer(Lexer):
 
     tokens = {
 
-        # Reserved Keywords:
-        CONST       , # 'const'
-        VAR         , # 'var'
-        PRINT       , # 'print'
-        RETURN      , # 'return'
-        BREAK       , # 'break'
-        CONTINUE    , # 'continue'
-        IF          , # 'if'
-        ELSE        , # 'else'
-        WHILE       , # 'while'
-        FUNC        , # 'func'
-        IMPORT      , # 'import'
-        TRUE        , # 'true'
-        FALSE       , # 'false'
+        # Keywords:
+        KEYWORD     ,
+        # 'const'
+        # 'var'
+        # 'print'
+        # 'return'
+        # 'break'
+        # 'continue'
+        # 'if'
+        # 'else'
+        # 'while'
+        # 'func'
+        # 'import'
+        # 'true'
+        # 'false'
+        # 'int'
+        # 'float'
 
         # Identifiers:
         ID          , # Text starting with a letter or '_', followed by any number
@@ -29,86 +34,90 @@ class WabbitLexer(Lexer):
                       # Examples:  'abc' 'ABC' 'abc123' '_abc' 'a_b_c'
 
         # Literals:
-        INT         , #  123   (decimal)
-        FLOAT       , # 1.234
+        FLOAT       , #  123   (decimal)
+        INT         , # 1.234
         CHAR        , # 'a'     (a single character - byte)
 
         # Operators:
-        PLUS         , # '+'
-        MINUS        , # '-'
-        STAR         , # '*'
-        SLASH        , # '/'
-        LT           , # '<'
-        LE           , # '<='
-        GT           , # '>'
-        GE           , # '>='
-        EQ           , # '=='
-        NE           , # '!='
-        LAND         , # '&&'
-        LOR          , # '||'
-        LNOT         , # '!'
-        GROW         , # '^'
+        PLUS        , # '+'
+        MINUS       , # '-'
+        STAR        , # '*'
+        SLASH       , # '/'
+        LT          , # '<'
+        LE          , # '<='
+        GT          , # '>'
+        GE          , # '>='
+        EQ          , # '=='
+        NE          , # '!='
+        LAND        , # '&&'
+        LOR         , # '||'
+        LNOT        , # '!'
+        GROW        , # '^'
+
+        SYNTAX      ,
 
         # Miscellaneous Symbols
-        ASSIGN       , # '='
-        SEMI         , # ';'
-        LPAREN       , # '('
-        RPAREN       , # ')'
-        LBRACE       , # '{'
-        RBRACE       , # '}'
-        COMMA        , # ','
-        DEREF        , # '`'    (Backtick)
+        ASSIGN      , # '='
+        SEMI        , # ';'
+        LPAREN      , # '('
+        RPAREN      , # ')'
+        LBRACE      , # '{'
+        RBRACE      , # '}'
+        COMMA       , # ','
+        DEREF       , # '`'    (Backtick)
     }
     ignore = ' \t'
 
-    # Reserved Keywords:
-    CONST       = 'const'
-    VAR         = 'var'
-    PRINT       = 'print'
-    RETURN      = 'return'
-    BREAK       = 'break'
-    CONTINUE    = 'continue'
-    IF          = 'if'
-    ELSE        = 'else'
-    WHILE       = 'while'
-    FUNC        = 'func'
-    IMPORT      = 'import'
-    TRUE        = 'true'
-    FALSE       = 'false'
-
     # Identifiers:
-    ID          = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    ID              = r'[a-zA-Z_][a-zA-Z0-9_]*'
+
+    # Reserved Keywords:
+    ID['const']     = KEYWORD
+    ID['var']       = KEYWORD
+    ID['print']     = KEYWORD
+    ID['return']    = KEYWORD
+    ID['break']     = KEYWORD
+    ID['continue']  = KEYWORD
+    ID['if']        = KEYWORD
+    ID['else']      = KEYWORD
+    ID['while']     = KEYWORD
+    ID['func']      = KEYWORD
+    ID['import']    = KEYWORD
+    ID['true']      = KEYWORD
+    ID['false']     = KEYWORD
+    ID['int']       = KEYWORD
+    ID['float']     = KEYWORD
 
     # Literals
-    FLOAT       = r'\d+\.\d*|\.\d+'
-    INT         = r'\d+'
-    CHAR        = r'[A-Za-z0-9]'
+    FLOAT           = r'\d+\.\d*|\.\d+'
+    INT             = r'\d+'
+    CHAR            = r'[A-Za-z0-9]'
 
     # Operators:
-    PLUS        = r'\+'
-    MINUS       = r'-'
-    STAR        = r'\*'
-    SLASH       = r'/'
-    LT          = r'<'
-    LE          = r'<='
-    GT          = r'>'
-    GE          = r'>='
-    EQ          = r'=='
-    NE          = r'!='
-    LAND        = r'&&'
-    LOR         = r'\|\|'
-    LNOT        = r'!'
-    GROW        = r'\^'
+    PLUS            = r'\+'
+    MINUS           = r'-'
+    STAR            = r'\*'
+    SLASH           = r'/'
+    LT              = r'<'
+    LE              = r'<='
+    GT              = r'>'
+    GE              = r'>='
+    EQ              = r'=='
+    NE              = r'!='
+    LAND            = r'&&'
+    LOR             = r'\|\|'
+    LNOT            = r'!'
+    GROW            = r'\^'
 
     # Miscellaneous Symbols
-    ASSIGN      = r'='
-    SEMI        = r';'
-    LPAREN      = r'\('
-    RPAREN      = r'\)'
-    LBRACE      = r'{'
-    RBRACE      = r'}'
-    COMMA       = r','
-    DEREF       = r'`'
+    ASSIGN          = r'='
+    SEMI            = r';'
+    LPAREN          = r'\('
+    RPAREN          = r'\)'
+    LBRACE          = r'{'
+    RBRACE          = r'}'
+    COMMA           = r','
+    DEREF           = r'`'
 
     # Ignored pattern
     ignore_newline = r'\n+'
@@ -120,3 +129,25 @@ class WabbitLexer(Lexer):
     def error(self, t):
         print("Illegal character '%s'" % t.value[0])
         self.index += 1
+
+def main(args):
+    '''
+var int x = 3;
+if x < 4 {
+    var y int = x + 2;
+    print y;
+}
+else {
+    var z float = x * 4 + 3 - 2;
+    print z;
+}
+    '''
+
+    print(main.__doc__)
+    lexer = WabbitLexer()
+    tokens = lexer.tokenize(main.__doc__)
+    for token in tokens:
+        print(token)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
